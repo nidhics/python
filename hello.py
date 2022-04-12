0e3a5293ecd2317da099f16dc4a0dcec47aad0fa
Nidhi Gupta <nidhitech0510@gmail.com>
2022-04-12 21:30:50 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,41 @@
-print("hello")
-print(2+3)
-print ("at last")
-print("o")
-print("hi")
+# print("hello")
+# print(2+3)
+# print ("at last")
+# print("o")
+# print("hi")
+# from random import shuffle
+
+# arr=[34,343,5456,65,76878,99,425]
+# shuffle(arr)
+# print(arr)
+# print(arr.pop())
+
+class Car:
+
+    def __init__(self,type,name, color,price) :
+        self.type=type
+        self.name=name
+        self.color=color
+        self.price=price
+
+    def dream_car(self):
+        # print(f"One day i will get {self.name}")
+        print("One day i will buy {} of {} color".format(self.name,self.color))
+        #format( function ) 
+
+    def __add__(self,other):
+        return self.type+ " " +other.type
+
+    def __lt__(self, other):
+        return self.price < other.price
+
+    def __gt__(self,other):
+        return self.price > other.price
+
+c1=Car("Electric","Nano","white",300000)
+print(dir(c1))#to get all dunder / magic method
+c1.dream_car()#One day i will buy Nano of white color
+c2=Car("Desiel","Nano","silver",200000)
+print(c1+c2)#Electric Desiel
+print(c1<c2)#False
+print(c1>c2)#True
